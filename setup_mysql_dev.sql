9aa0ce7e977436d3a27a7a6efb64c1f73358f56a
mugane <muganes25@gmail.com>
2023-04-19 01:06:19 -0700

    create db

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,4 @@
+--- Creates db hbnb_dev_db and user hbnb_dev with all privileges
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
