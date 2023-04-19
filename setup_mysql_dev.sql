d84d5d8d7e19338eddad3aeb4474d8c50467946b
mugane <muganes25@gmail.com>
2023-04-19 01:09:32 -0700

    create db

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,3 +5,4 @@ USE hbnb_dev_db;
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
 USE performance_schema;
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
