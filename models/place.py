905f8e662ebfdd05969e733b03f947446e3fdb16
mugane <muganes25@gmail.com>
2023-04-19 05:00:29 -0700

    attr updates

--- models/place.py
+++ models/place.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import Base
-from models.base_model import BasseModel
+from models.base_model import BaseModel
 from sqlalchemy import Column, Integer, String, ForeignKey, Float
 
 
