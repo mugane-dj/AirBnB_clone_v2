0665d2c9c06a2662bf7376ade6471f8f00427424
mugane <muganes25@gmail.com>
2023-04-22 01:50:58 -0700

    place model

--- models/place.py
+++ models/place.py
@@ -28,10 +28,8 @@ class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False,
-                         unique=True)
-        user_id = Column(String(60), ForeignKey("users.id"), nullable=False,
-                         unique=True)
+        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, default=0, nullable=False)
