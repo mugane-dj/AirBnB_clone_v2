d3802786b6ad704ef8f64dcfb634a8a1f68e0d40
mugane <muganes25@gmail.com>
2023-04-22 01:48:55 -0700

    place model

--- models/place.py
+++ models/place.py
@@ -3,7 +3,14 @@
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
-from sqlalchemy import Column, Integer, String, ForeignKey, Float, Table
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    ForeignKey,
+    Float,
+    Table,
+)
 from sqlalchemy.orm import relationship
 
 place_amenity = Table(
@@ -21,8 +28,10 @@ class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False,
+                         unique=True)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False,
+                         unique=True)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, default=0, nullable=False)
