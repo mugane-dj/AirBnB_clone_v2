c9e9baab2b634a6d2fbb654784b8c028138ea5d9
mugane <muganes25@gmail.com>
2023-05-04 05:09:23 -0700

    0x1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,13 +4,13 @@ Fabfile to:
     - generate a .tgz archive from the contents of the web_static folder
 """
 
-from fabric import *
+from fabric.api import local
 from datetime import datetime
 
 
 def do_pack():
     """Generate .tgz archive"""
-    sudo("mkdir -p versions")
+    local("mkdir -p versions")
     _datetime = datetime.now()
     archive = "web_static_" + _datetime.strftime("%Y%m%d%H%M%S") + ".tgz"
     result = local("tar -cvzf versions/{} web_static".format(archive))
