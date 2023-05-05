6ef674790cdc0d3b9924606e9515500312dd9336
mugane <muganes25@gmail.com>
2023-05-05 05:30:48 -0700

    0x2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -18,24 +18,30 @@ env.hosts = [
 def do_deploy(archive_path):
     """Uploads we_static to servers"""
     if os.path.isfile(archive_path):
-        try:
-            file_name_ext = archive_path.split("/")[-1]
-            file_name = file_name_ext.split(".")[0]
+        file_name_ext = archive_path.split("/")[-1]
+        file_name = file_name_ext.split(".")[0]
 
-            path_ext = "/tmp/{}".format(file_name_ext)
-            path = "/data/web_static/releases/{}/".format(file_name)
-
-            put(archive_path, "/tmp/")
-            run("rm -rf {}".format(path))
-            run("mkdir -p {}".format(path))
-            run("tar -xzf {} -C {}".format(path_ext, path))
-            run("rm -rf {}".format(path_ext))
-            run("mv {}/web_static/* {}".format(path, path))
-            run("rm -rf {}/web_static".format(path))
-            run("rm -rf /data/web_static/current")
-            run("ln -s {} /data/web_static/current".format(path))
-            return True
-        except Exception:
+        path_ext = "/tmp/{}".format(file_name_ext)
+        path = "/data/web_static/releases/{}/".format(file_name)
+        sym_link = "/data/web_static/current"
+        if put(archive_path, "/tmp/").failed is True:
             return False
+        if run("rm -rf {}".format(path)).failed is True:
+            return False
+        if run("mkdir -p {}".format(path)).failed is True:
+            return False
+        if run("tar -xzf {} -C {}".format(path_ext, path)).failed is True:
+            return False
+        if run("rm -rf {}".format(path_ext)).failed is True:
+            return False
+        if run("mv {}web_static/* {}".format(path, path)).failed is True:
+            return False
+        if run("rm -rf {}/web_static".format(path)).failed is True:
+            return False
+        if run("rm -rf /data/web_static/current").failed is True:
+            return False
+        if run("ln -s {} {}".format(path, sym_link)).failed is True:
+            return False
+        return True
     else:
         return False
