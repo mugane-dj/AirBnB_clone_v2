1af6912e7ed1ac55d3823e1b7b3166efd1365653
mugane <muganes25@gmail.com>
2023-05-05 04:41:43 -0700

    0x2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,14 +19,18 @@ def do_deploy(archive_path):
     """Uploads we_static to servers"""
     if os.path.exists(archive_path):
         try:
+            file_name_ext = archive_path.split("/")[-1]
+            file_name = file_name_ext.split(".")[0]
             put(archive_path, "/tmp/")
-            path = "/tmp/{}".format(archive_path.split("/")[-1])
-            run("tar -xvzf {} -C /data/web_static/releases/".format(path))
-            with cd("/tmp"):
-                run("rm -rf {}".format(path))
-            run("rm /data/web_static/current")
-            link = "/data/web_static/current"
-            run("ln -sf {} /data/web_static/releases".format(link))
+            path_ext = "/tmp/{}".format(file_name_ext)
+            path = "/data/web_static/releases/{}/".format(file_name)
+            run("mkdir -p {}".format(path))
+            run("tar -xzf {} -C {}".format(path_ext, path))
+            run("rm -rf {}".format(path_ext))
+            run("mv {}/web_static/* {}".format(path, path))
+            run("rm -rf {}/web_static".format(path))
+            run("rm -rf /data/web_static/current")
+            run("ln -sf {} /data/web_static/current".format(path))
             return True
         except Exception:
             return False
