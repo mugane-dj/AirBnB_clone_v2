e91cf00d35c287cce3e672e2a842d2fa040b85c6
mugane <muganes25@gmail.com>
2023-05-20 05:12:49 -0700

    models update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -63,3 +63,8 @@ class FileStorage:
             key = str(obj.__class__.__name__) + "." + str(obj.id)
             FileStorage.__objects.pop(key)
             self.save()
+
+
+    def close(self);
+        """Calls reload method for deserializing the JSON file to objects"""
+        self.reload()
--- models/state.py
+++ models/state.py
@@ -2,7 +2,9 @@
 """ State Module for HBNB project """
 from os import getenv
 from models.base_model import BaseModel, Base
+from models.city import City
 from sqlalchemy import Column, Integer, String, ForeignKey
+from models.engine.file_storage import FileStorage
 
 
 class State(BaseModel, Base):
@@ -12,3 +14,7 @@ class State(BaseModel, Base):
         name = Column(String(128), nullable=False)
     else:
         name = ""
+
+        @property
+        def cities(self):
+            pass
