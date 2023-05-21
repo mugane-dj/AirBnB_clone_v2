c5f634451fc1bf9fc8cd216adb82ae63b306135c
mugane <muganes25@gmail.com>
2023-05-21 03:58:27 -0700

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -22,7 +22,7 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        
+
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
