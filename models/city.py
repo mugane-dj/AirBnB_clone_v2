d77ffb369f798524f8e6f0a549582ddefa08cc38
mugane <muganes25@gmail.com>
2023-05-21 03:06:34 -0700

    city model

--- models/city.py
+++ models/city.py
@@ -2,7 +2,6 @@
 """ City Module for HBNB project """
 from os import getenv
 from models.base_model import BaseModel, Base
-from models.state import State
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
