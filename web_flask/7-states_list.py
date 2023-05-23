246131260f942f51d25cc69609b008fd51f234f6
mugane-dj <muganes25@gmail.com>
2023-05-23 17:03:35 +0000

    0x8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Module contains a script that starts a Flask web application"""
+"""
+Module contains a script that starts a Flask web application
+"""
 
 
 import models
@@ -12,10 +14,14 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    states = list(models.storage.all(State).values())
-    print(states)
-    sorted_states = sorted(states, key=lambda x: x["name"])
+    states = list(models.storage.all('State').values())
+    sorted_states = sorted(states, key=lambda state: state.name)
     return render_template('7-states_list.html', states=sorted_states)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(self):
+    models.storage.close()
 
 
 if __name__ == '__main__':
